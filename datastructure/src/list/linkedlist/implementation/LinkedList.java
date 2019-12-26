4eb3d0db8b0a624ebf86e86bc65eb2a12c41c487
Leewontak <takgung6820@gmail.com>
2019-12-26 15:57:47 +0900

    Create file

---
+++ datastructure/src/list/linkedlist/implementation/LinkedList.java
@@ -0,0 +1,15 @@
+package list.linkedlist.implementation;
+
+import javafx.scene.Node;
+
+public class LinkedList {
+    // 첫 번째 노드를 가리키는 필드
+    private Node head;
+    private Node tail;
+    private int size = 0;
+    private class node {
+        // 데이터가 저장될 필드
+        public Object data;
+        // 다음 노드를 가리키는 필드
+    }
+}
